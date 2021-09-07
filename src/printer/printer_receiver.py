bf7c152fb435bc20505b8e74229cc0a3472a1053
Gabriel Musat <gabriel.musat@dive.tech>
2021-09-07 08:01:32 +0200

    refactor: removed unused code

--- src/printer/printer_receiver.py
+++ src/printer/printer_receiver.py
@@ -1,7 +1,5 @@
 import asyncio
 import copy
-import json
-import os
 import time
 from abc import ABC, abstractmethod
 from typing import Dict, Any
@@ -21,12 +19,14 @@ class PrinterReceiver(ABC):
                  octo_api: OctoApi,
                  ucloud_api: UcloudApi,
                  upload_path: str,
+                 retry_timeout: int = 20,
                  ping_timeout: int = 10
                  ):
         self.sentState: Dict[str, Any] = {}
         self.actualState: Dict[str, Any] = {}
         self.upload_path = upload_path
         self.ping_timeout = ping_timeout
+        self.retry_timeout = retry_timeout
         self.connected = False
         self.transmit = False
         self.octo_api = octo_api
@@ -83,7 +83,7 @@ class PrinterReceiver(ABC):
                 status = self.actualState["status"]["state"]["text"]
 
             error = self.actualState["error"] if "error" in self.actualState else 0
-            if time.time() - last_connection_try > 20 and (status == 'Closed' or error == 409):
+            if time.time() - last_connection_try > self.retry_timeout and (status == 'Closed' or error == 409):
                 log.warning("closed status detected, forcing connection...")
                 last_connection_try = time.time()
                 try:
@@ -91,7 +91,7 @@ class PrinterReceiver(ABC):
                     log.info("printer connected correctly")
 
                 except HttpException:
-                    log.warning("could not connect printer, trying again in {20}s")
+                    log.warning(f"could not connect printer, trying again in {self.retry_timeout}s")
 
                 except ClientConnectorError as e:
                     log.error("client connection error with octoprint server while trying to connect printer: "+str(e))
@@ -110,20 +110,11 @@ class PrinterReceiver(ABC):
 
     async def init(self):
         self.sentState = {}
-        if os.path.isfile("../store.json"):
-            self.actualState = {"settings": json.load(open("../store.json"))}
-        else:
-            self.actualState = {"settings": {"init_gcode": "M851 Z-0.5"}}
-            json.dump(self.actualState['settings'], open("../store.json", "w"))
-        self.actualState['download'] = {'file': None, 'completion': -1}
-        self.actualState['error'] = None
-
+        self.actualState = {
+            'download': {'file': None, 'completion': -1},
+            'error': None
+        }
         await self.updateActualState()
-
-    async def updateSettings(self, spec: Dict):
-        for k in spec:
-            self.actualState["settings"][k] = spec[k]
-        json.dump(self.actualState["settings"], open("../store.json", "w"))
 
     async def updateActualState(self):
         try:
@@ -144,24 +135,6 @@ class PrinterReceiver(ABC):
             log.error("unknown error updating status: "+str(e))
             self.actualState["status"] = {"state": {"text": "Disconnected"}}
             self.actualState["error"] = 500
-
-    async def _updateActualState(self):
-        try:
-            self.actualState["status"] = await self.octo_api.get_status()
-            self.actualState["job"] = await self.octo_api.get_job()
-            if self.actualState["status"]["state"]['text'] == 'Closed':
-                log.warning("closed status detected, forcing connection...")
-                await self.octo_api.connect()
-
-        except HttpException as e:
-            self.actualState["status"] = {"state": {"text": "Disconnected"}}
-            if e.code == 409:
-                log.warning("octoprint returned 409 while requesting status, forcing connection...")
-                await self.octo_api.connect()
-
-        except ClientConnectorError as e:
-            log.error("error connecting to octoprint: "+str(e))
-            self.actualState["status"] = {"state": {"text": "Disconnected"}}
 
     async def sync(self):
         spec = diff_engine.diff(self.actualState, self.sentState)
